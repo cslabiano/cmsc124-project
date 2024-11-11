52b743809d5b8063780c5c86d6eaf36e00f34a82
cslabiano <cslabiano@up.edu.ph>
2024-11-11 13:49:32 +0800

    make text editor editable and reflect changes to the file

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # cmsc124-project
+
 LOLCode Interpreter
+
+<!-- to be deleted before final submission -->
+
+references I used:
+
+- [PyQt5 open File Dialog and read file or ( browse File)](https://www.youtube.com/watch?v=uXlL2PeuLpQ)
+- [File Dialog Boxes With QFileDialog - PyQt5 GUI Thursdays #29](https://www.youtube.com/watch?v=gg5TepTc2Jg)
--- main.py
+++ main.py
@@ -10,24 +10,37 @@ class UI(QMainWindow):
     uic.loadUi("gui.ui", self)
 
     # define widgets
-    self.button = self.findChild(QPushButton, "open_file_button")
-    self.label = self.findChild(QTextEdit, "print_file_name")
+    self.open_button = self.findChild(QPushButton, "open_file_button")
+    self.text_editor = self.findChild(QTextEdit, "print_file_name")
+    self.execute_button = self.findChild(QPushButton, "execute_button")
 
     # call widget functions
-    self.button.clicked.connect(self.clicker)
+    self.open_button.clicked.connect(self.open_file)
+    self.execute_button.clicked.connect(self.save_file)
+
+    # initialize the file path
+    self.file_path = None
 
     # shows the app
     self.show()
   
-  def clicker(self):
+  def open_file(self):
     # self.label.setText("You clicked the button!")
     fname = QFileDialog.getOpenFileName(self, "Select File", "", "LOLCode Files (*.lol)")
-    path = fname[0]
+    self.file_path = fname[0]
 
-    with open(path, "r") as f:
-      content = f.read()  # Read the entire file content
-      self.label.setText(content)  # Display the content in the label
+    with open(self.file_path, "r") as f:
+      content = f.read()  # read the entire file content
+      self.text_editor.setText(content)  # display the content in the label
 
+  def save_file(self):
+    # check if there's a file opened
+    if self.file_path:
+      with open(self.file_path, "w") as f:
+        content = self.text_editor.toPlainText()  # get content from QTextEdit
+        f.write(content)  # write content back to the file
+    else:
+      print("No file opened to save.")
        
 
 # initialize the app
