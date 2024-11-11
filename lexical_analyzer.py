c65d983445adba68a74ed69b25b55218e84e438c
cslabiano <cslabiano@up.edu.ph>
2024-11-11 22:57:21 +0800

    display lexical analyzer results in gui

---
+++ lexical_analyzer.py
@@ -0,0 +1,11 @@
+def analyze_lexemes(content):
+
+  # sample output only
+  lexemes = [("HELLO", "Keyword"),
+    ("WORLD", "Identifier"),
+    ("42", "Literal"),
+    ("PRINT", "Function"),
+    ("VAR", "Variable"),
+    ("=", "Operator")]
+  
+  return lexemes
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from PyQt5.QtWidgets import QMainWindow, QApplication, QPushButton, QTextEdit, QFileDialog
+from PyQt5.QtWidgets import QMainWindow, QApplication, QPushButton, QTextEdit, QFileDialog, QLabel, QTableWidget, QTableWidgetItem
 from PyQt5 import uic
 import sys
+import lexical_analyzer
 
 class UI(QMainWindow):
   def __init__(self):
@@ -13,10 +14,11 @@ class UI(QMainWindow):
     self.open_button = self.findChild(QPushButton, "open_file_button")
     self.text_editor = self.findChild(QTextEdit, "print_file")
     self.execute_button = self.findChild(QPushButton, "execute_button")
+    self.lexeme_table = self.findChild(QTableWidget, "lexeme_table")
 
     # call widget functions
     self.open_button.clicked.connect(self.open_file)
-    self.execute_button.clicked.connect(self.save_file)
+    self.execute_button.clicked.connect(self.execute)
 
     # initialize the file path
     self.file_path = None
@@ -33,15 +35,26 @@ class UI(QMainWindow):
       content = f.read()  # read the entire file content
       self.text_editor.setText(content)  # display the content in the label
 
-  def save_file(self):
-    # check if there's a file opened
-    if self.file_path:
-      with open(self.file_path, "w") as f:
-        content = self.text_editor.toPlainText()  # get content from QTextEdit
-        f.write(content)  # write content back to the file
-    else:
-      print("No file opened to save.")
-       
+  def execute(self):
+    # get the current content of the text editor after clicking the execute button
+    content = self.text_editor.toPlainText()
+
+    # if file is not empty, perform lexeme analysis
+    if content:
+      lexemes = lexical_analyzer.analyze_lexemes(content)
+      self.lexeme_table.clearContents()   # clear previous contents of the table
+
+      self.lexeme_table.setRowCount(len(lexemes))   # get the length of the lexemes and set it as the number of rows of the table widget
+
+      # populate the table
+      for row, (lexeme, classification) in enumerate(lexemes):
+        self.lexeme_table.setItem(row, 0, QTableWidgetItem(lexeme))
+        self.lexeme_table.setItem(row, 1, QTableWidgetItem(classification))
+
+      # changes made in the text editor will be saved in the original file
+      if self.file_path:
+        with open(self.file_path, "w") as f:
+          f.write(content)
 
 # initialize the app
 app = QApplication(sys.argv)
