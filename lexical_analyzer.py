ece868d8a6ab35de3f9f6b819c7a929a39af32bf
MarkusJakobR <mcrecaplaza@up.edu.ph>
2024-11-12 10:41:08 +0800

    added lexical analyzer function but needs fixing

--- lexical_analyzer.py
+++ lexical_analyzer.py
@@ -1,11 +1,32 @@
+import re
+
 def analyze_lexemes(content):
 
-  # sample output only
-  lexemes = [("HELLO", "Keyword"),
-    ("WORLD", "Identifier"),
-    ("42", "Literal"),
-    ("PRINT", "Function"),
-    ("VAR", "Variable"),
-    ("=", "Operator")]
+  lexemes = []
+  pattern_dict = {}
+
+  #read the patterns.txt file
+  patterns = open("patterns.txt", "r")
+
+  if not patterns:
+    print("Error in patterns.txt")
+
+  lines = patterns.readlines()
+
+  #place patterns to a dictionary
+  for line in lines:
+    parts = line.strip().split(", ")
+    #remove "" of the regex
+    pattern = parts[0].strip('"')
+    type = parts[1]
+    #use re.compile() to treat it as r"pattern"
+    pattern_dict[re.compile(pattern)] = type
+
+  #find matches per line of the content
+  for line in content.splitlines():
+    for regex, type in pattern_dict.items():
+      matches = regex.findall(line)
+      for match in matches:
+        lexemes.append((match, type))
   
   return lexemes
