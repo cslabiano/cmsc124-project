58b09fcf5f740263a9caf6c6a6a558a5af786265
ReddDL <redeleon.dev@gmail.com>
2024-11-13 13:22:10 +0800

    return false on invalid lexeme

--- lexical_analyzer.py
+++ lexical_analyzer.py
@@ -41,21 +41,29 @@ def analyze_lexemes(content):
           if match:
             #takes the whole match
             lexeme = match.group(0)
+            # If the type match is a Comment Delimiter
             if type == "Comment Delimiter":
               lexemes.append((lexeme, type))
               line = line.replace(lexeme, "", 1).strip()
+              # Add to tokens the rest of the line 
               if line:
                 lexemes.append((line, 'Comment'))
                 line = ""
+            # If the type match is a Multiline Comment Delimiter
             elif type == "Multiline Comment Delimiter":
                 lexemes.append((lexeme, type))
                 insideMultilineComment = not insideMultilineComment
                 line = line.replace(lexeme, "", 1).strip()
+            # If the type match is a YARN literal
             elif type == "YARN Literal":
+                # Append quote
                 lexemes.append(('"', 'String Delimiter'))
+                # Append YARN literal
                 lexemes.append((lexeme[1:-1], type))
+                # Append quote
                 lexemes.append(('"', 'String Delimiter'))
                 line = line.replace(lexeme, "", 1).strip()
+            # Append to tokens normally
             else: 
               lexemes.append((lexeme, type))
               #removes the match in the current line ensuring that it won't repeat
@@ -64,6 +72,6 @@ def analyze_lexemes(content):
             break
       #in case nothing matches
       if not matched:
-          break 
+          return False
   
   return lexemes
